c6195172f8fee9f697a3e40462ef2cfa5ada3c74
Annaurna <prathipati.annapurna@gmail.com>
2026-02-18 11:55:32 +0530

    Update Program.cs

--- UserDirectory.Api/Program.cs
+++ UserDirectory.Api/Program.cs
@@ -2,6 +2,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+// Add Entity Framework Core
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngular",
